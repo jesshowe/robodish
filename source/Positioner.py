b4ee0ab2123078dbeca9e5d88b682263d6dc9df7
Jess Howe <jess@byu.net>
2017-10-27 20:10:55 +0000

    Looking into reading satellite info from file

--- source/Positioner.py
+++ source/Positioner.py
@@ -1,5 +1,17 @@
 import serial
-ser = serial.Serial('/dev/ttyACM0')
+import json
 
-def writeSatellites
-def readSatellites
+class Positioner:
+    ser = serial.Serial('/dev/ttyACM0')
+    def writeSatellites(self):
+
+    def readSatellites(self):
+        with open('satellites.txt') as json_file:
+            data = json.load(json_file)
+            for satellite in data['satellite']:
+
+    def gotoSatellite(SatelliteName):
+        #Look up SatelliteName in db
+        #Figure out where we are first
+        #Figure out how many ticks it will take to get to SatelliteName
+        #
