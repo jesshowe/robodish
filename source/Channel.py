4cfd014027649566fab50a89668ad0ba1d1a8ca3
Jess Howe <jess@byu.net>
2017-10-27 20:10:29 +0000

    Initial checkin

---
+++ source/Channel.py
@@ -0,0 +1,14 @@
+class Channel:
+    def __init__(self):
+        self.Name = None
+        self.Frequency = None
+        self.SymbolRate = None
+        self.FEC = None
+        self.Encryption = None
+        self.SID = None
+        self.VPID = None
+        self.APIDList = None
+        self.Satellite = None
+        self.Polarity = None
+        self.Modulation = None
+        self.DateAdded = None
