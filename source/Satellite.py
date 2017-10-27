719d5c1a54ab6dfd8ab104010f87040a2f780d9d
Jess Howe <jess@byu.net>
2017-10-27 20:10:19 +0000

    Initial checkin

---
+++ source/Satellite.py
@@ -0,0 +1,10 @@
+class Satellite:
+    def __init__(self):
+        self.Name = None
+        self.OrbitalLocation = None
+        self.TickLocation = None
+        self.ChannelList = None
+        self.Band = None
+        self.DateAdded = None
+        self.next = None
+        self.prev = None
